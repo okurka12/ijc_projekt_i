453464b74b6cc924e43022fa69b4bcb5d10a3270
Vit Pavlik <pavlikvitek13@gmail.com>
2023-02-26 05:39:36 +0100

    modified error.h

--- error.h
+++ error.h
@@ -10,22 +10,12 @@
 **  2023-02-26  **
 *****************/
 
-
-
-/* 
-   Napište modul "error.c" s rozhraním v "error.h", který definuje
-   funkci void warning(const char *fmt, ...) a
-   funkci void error_exit(const char *fmt, ...).
-   Tyto funkce mají stejné parametry jako printf(); tisknou
-   text "Warning: " nebo "Error: " a potom chybové hlášení podle
-   formátu fmt. Vše se tiskne do stderr (standardní funkcí vfprintf)
-   a potom pouze error_exit ukončí program voláním funkce exit(1).
-   Použijte definice ze stdarg.h.
-*/
-
+#ifndef _ERROR_H
+#define _ERROR_H
 /* Tiskne warning do stderr, voláno stejně jako printf() */
 void warning(const char *fmt, ...);
 
 
 /* Tiskne error do stderr a ukončí program, voláno stejně jako printf() */
 void error_exit(const char *fmt, ...);
+#endif
