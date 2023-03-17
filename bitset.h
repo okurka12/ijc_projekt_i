f97af29fbd5b1878cc20a9285204feec27924557
Vit Pavlik <pavlikvitek13@gmail.com>
2023-03-17 16:15:07 +0100

    inline definitions

--- bitset.h
+++ bitset.h
@@ -24,6 +24,7 @@
 
 #include "error.h"
 
+#ifndef _BITSET_H
 
 typedef unsigned long bitset_index_t;
 typedef unsigned long bitset_t;
@@ -36,17 +37,15 @@ typedef unsigned long bitset_t;
 #define SULB sizeof(unsigned long) * 8
 
 
-#ifndef USE_INLINE
-
-
 /* definuje a nuluje proměnnou jmeno_pole */
 #define bitset_create(jmeno, velikost) \
     static_assert(velikost > 0 && velikost <= ULONG_MAX, /* xxx */ \
-                  "bitset_create: Nesprávná velikost pole " \
-                  "(parametr `velikost` je počet bitů."); \
+                  "bitset_create: Nespravna velikost pole " \
+                  "(parametr `velikost` je pocet bitu."); \
     bitset_t jmeno[bity(velikost) + 2] = {0}; \
     jmeno[0] = velikost;
 
+#ifndef USE_INLINE
 
 /* definuje proměnnou jmeno_pole tak, aby byla kompatibilní s polem
    vytvořeným pomocí bitset_create, ale pole je alokováno dynamicky */
@@ -73,24 +72,23 @@ typedef unsigned long bitset_t;
 #define bitset_setbit(jmeno, index, vyraz) \
     if (index > jmeno[0] - 1) { \
         error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu\n", \
-                   (unsigned long)index, jmeno[0]); \
+                   (bitset_t)index, jmeno[0]); \
     } \
 \
     if (vyraz) { \
         /*   [index v poli UL] |=  (maska <<  (index v konkrétním UL)) */ \
         jmeno[bity(index) + 1] |=  (1UL   <<  (index % SULB)); \
-    } \
-    else { \
+    } else { \
         jmeno[bity(index) + 1] &= ~(1UL   <<  (index % SULB)); \
     }
 
 
-// takto by to asi melo byt hotove ale radeji zkontrolovat ja uz na to nemam ted
+/* získá hodnotu zadaného bitu, vrací hodnotu 0 nebo 1 */
 #define bitset_getbit(jmeno, index) \
     (index > jmeno[0] - 1) ? \
         ( \
             error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu\n", \
-                       (unsigned long)index, jmeno[0]),  /* operator carka */ \
+                       (bitset_t)index, jmeno[0]),  /* operator carka */ \
             0 \
         ) \
     : \
@@ -104,7 +102,61 @@ typedef unsigned long bitset_t;
      * jako ja
      * */
 
+#else  // ndef USE_INLINE
+
+/* definuje proměnnou jmeno_pole tak, aby byla kompatibilní s polem
+   vytvořeným pomocí bitset_create, ale pole je alokováno dynamicky */
+#define bitset_alloc(jmeno, velikost) bitset_t *jmeno = _bitset_alloc(velikost)
 
 
-// TODO: use inline !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#endif
+static inline bitset_t* _bitset_alloc(bitset_t velikost) {
+    assert(velikost > 0 && velikost <= ULONG_MAX);
+    bitset_t *output;
+    if ((output = malloc(bity(velikost) + 2)) == NULL) {
+        error_exit("bitset_alloc: Chyba alokace paměti.\n");
+    }
+    return output;
+}
+
+
+/* uvolní paměť dynamicky (bitset_alloc) alokovaného pole */
+static inline void bitset_free(bitset_t *jmeno) {
+    free(jmeno);
+}
+
+
+/* vrátí deklarovanou velikost pole v bitech */
+static inline bitset_t bitset_size(bitset_t jmeno[]) {
+    return jmeno[0];
+}
+
+
+/* nastaví zadaný bit v poli na hodnotu zadanou výrazem
+   (nulový výraz == bit 0, nenulový výraz == bit 1) */
+static inline void bitset_setbit(bitset_t jmeno[], bitset_t index, int vyraz) {
+    if (index > jmeno[0] - 1) {
+        error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu\n",
+                   (unsigned long)index, jmeno[0]);
+    }
+
+    if (vyraz) {
+        /*   [index v poli UL] |=  (maska <<  (index v konkrétním UL)) */
+        jmeno[bity(index) + 1] |=  (1UL   <<  (index % SULB));
+    } else {
+        jmeno[bity(index) + 1] &= ~(1UL   <<  (index % SULB));
+    }
+}
+
+
+/* získá hodnotu zadaného bitu, vrací hodnotu 0 nebo 1 */
+static inline bitset_t bitset_getbit(bitset_t jmeno[], bitset_t index) {
+    if (index > jmeno[0] - 1) {
+        error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu\n",
+                   index, jmeno[0]);
+
+    }
+    return ((jmeno[bity(index) + 1]) >> (index % SULB)) & 1UL;
+}
+
+#endif  // ndef USE_INLINE
+#endif  // ndef _BITSET_H
